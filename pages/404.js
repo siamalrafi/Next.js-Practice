3b217e850b9eaeff6f282b7a1cefc505bc1b553c
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-01-04 23:10:08 +0600

    This is th enext

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+import { useRouter } from "next/router";
+
+const Error = () => {
+    // const router = useRouter();
+    // const id = router.query;
+    // console.log(id); // $$$ NOT AVAILABLE!
+    
+    return (
+        <div>
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, itaque exercitationem repellendus reiciendis, praesentium inventore laudantium in mollitia animi enim ex, explicabo sapiente eveniet atque repellat eius. Dolore facere neque obcaecati, fuga molestiae aliquid provident maiores esse ad magni sint dolor tenetur vero inventore voluptatem officia rem, nulla voluptas hic.</h1>
+        </div>
+    );
+};
+
+export default Error;
